030a09a718d9e7980e50b0ea3c22f16e5cc2517c
C <chrismatusemail@gmail.com>
2020-04-09 10:27:48 +0100

    fixed heroku toggle

--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@ import ReactDOM from "react-dom";
 import App from "./components/App.jsx";
 import socketIOClient from "socket.io-client";
 
-let shouldEndpointBeHeroku = true //TOGGLE THIS MANUALLY DURING DEVELOPMENT
+let shouldEndpointBeHeroku = false; //TOGGLE THIS MANUALLY DURING DEVELOPMENT
 
-const socket
-socket = shouldEndpointBeHeroku ? socketIOClient("https://wormplayserver.herokuapp.com/") : socketIOClient("http://localhost:4001");
+const socket = socketIOClient(
+  shouldEndpointBeHeroku
+    ? "https://wormplayserver.herokuapp.com/"
+    : "http://localhost:4001"
+);
 
 ReactDOM.render(<App socket={socket} />, document.getElementById("root"));
