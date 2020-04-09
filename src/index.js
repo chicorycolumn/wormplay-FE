377d021457aa673acc2466efc5c0cfd2b0a611ee
C <chrismatusemail@gmail.com>
2020-04-09 10:21:53 +0100

    Added toggle for using heroku or localhost.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App.jsx";
 import socketIOClient from "socket.io-client";
-const socket = socketIOClient("https://wormplayserver.herokuapp.com/");
+
+let shouldEndpointBeHeroku = true //TOGGLE THIS MANUALLY DURING DEVELOPMENT
+
+const socket
+socket = shouldEndpointBeHeroku ? socketIOClient("https://wormplayserver.herokuapp.com/") : socketIOClient("http://localhost:4001");
 
 ReactDOM.render(<App socket={socket} />, document.getElementById("root"));
