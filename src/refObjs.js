717921d58f0440d10636e23fbbf0618f38b04660
C <chrismatusemail@gmail.com>
2020-04-13 10:34:35 +0100

    refactor lettermaking

---
+++ src/refObjs.js
@@ -0,0 +1,27 @@
+const vowelArray = ["A", "E", "I", "O", "U"];
+
+const consonantArray = [
+  "B",
+  "C",
+  "D",
+  "F",
+  "G",
+  "H",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+
+module.exports = { vowelArray, consonantArray };
