ca16098dec4271bef25184cdacd8f62465632e81
C <chrismatusemail@gmail.com>
2020-04-16 07:48:16 +0100

    before moving georgine

--- src/components/Lobby.jsx
+++ src/components/Lobby.jsx
@@ -4,6 +4,100 @@ import styles from "./css/Lobby.module.css";
 
 import SidePanel from "./SidePanel.jsx";
 
+let exampleLobbyData = [
+  {
+    id: "gr8", //possible screw point
+    roomName: "dragon team",
+    passcode: "1234",
+    p1: { username: "lenka", id: "33e" },
+    p2: { username: "bazinski", id: "44r" },
+  },
+  {
+    id: "gr8", //possible screw point
+    roomName: "team",
+    passcode: "1234",
+    p1: { username: "l", id: "33e" },
+    p2: { username: "b", id: "44r" },
+  },
+  {
+    id: "gr8", //possible screw point
+    roomName: "dragon",
+    passcode: "1234",
+    p1: { username: "a", id: "33e" },
+    p2: { username: "i", id: "44r" },
+  },
+  {
+    id: "gr8", //possible screw point
+    roomName: "DT",
+    passcode: "1234",
+    p1: { username: "en", id: "33e" },
+    p2: { username: "az", id: "44r" },
+  },
+  {
+    id: "gr8", //possible screw point
+    roomName: "eam",
+    passcode: "1234",
+    p1: { username: "ka", id: "33e" },
+    p2: { username: "ki", id: "44r" },
+  },
+  {
+    id: "gr8", //possible screw point
+    roomName: "drag",
+    passcode: "1234",
+    p1: { username: "ll", id: "33e" },
+    p2: { username: "bb", id: "44r" },
+  },
+  {
+    id: "gr8", //possible screw point
+    roomName: "dragon team",
+    passcode: "1234",
+    p1: { username: "lenka", id: "33e" },
+    p2: { username: "bazinski", id: "44r" },
+  },
+  {
+    id: "gr8", //possible screw point
+    roomName: "dream",
+    passcode: "1234",
+    p1: { username: "la", id: "33e" },
+    p2: { username: "bi", id: "44r" },
+  },
+  {
+    id: "gr8", //possible screw point
+    roomName: "dr",
+    passcode: "1234",
+    p1: { username: "le", id: "33e" },
+    p2: { username: "ba", id: "44r" },
+  },
+  {
+    id: "gr8", //possible screw point
+    roomName: "dragon team",
+    passcode: "1234",
+    p1: { username: "lenka", id: "33e" },
+    p2: { username: "bazinski", id: "44r" },
+  },
+  {
+    id: "gr8", //possible screw point
+    roomName: "drago",
+    passcode: "1234",
+    p1: { username: "lenk", id: "33e" },
+    p2: { username: "bazin", id: "44r" },
+  },
+  {
+    id: "gr8", //possible screw point
+    roomName: "doctor",
+    passcode: "1234",
+    p1: { username: "lenkanda", id: "33e" },
+    p2: { username: "baz", id: "44r" },
+  },
+  {
+    id: "gr8", //possible screw point
+    roomName: "drm",
+    passcode: "1234",
+    p1: { username: "lea", id: "33e" },
+    p2: { username: "bai", id: "44r" },
+  },
+];
+
 export default class Lobby extends React.Component {
   constructor() {
     super();
